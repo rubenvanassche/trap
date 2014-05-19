e9c338501cb4a5b96930f544d23ec3fd9f48354b
Ruben Van Assche <rubenvanassche@gmail.com>
2014-05-19 21:52:37 +0200

    fix

--- index.php
+++
@@ -1,7 +0,0 @@
-<h1>U wordt doorgewezen</h1>
-<a href="http://www.scouts-zurenborg.be/trap/public/">http://www.scouts-zurenborg.be/trap/public/</a>
-<script type="text/javascript">
-<!--
-window.location = "http://www.scouts-zurenborg.be/trap/public/"
-//-->
-</script>
